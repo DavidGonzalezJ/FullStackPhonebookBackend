af9c2d049ff9c476f237b8bb79f3137de397264a
David <davidgonzalezj97@gmail.com>
2023-11-23 13:42:13 +0100

    Exercise 3.11 Making a production build and deploying the app to internet

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('post-body',(request,response)=>{
     if(request.method === 'POST')
