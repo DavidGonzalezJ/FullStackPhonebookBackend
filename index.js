1ed4694baa6e83e95696550d6a0821561793c13a
David <davidgonzalezj97@gmail.com>
2023-11-29 13:49:02 +0100

    Exercise 3.16 Implementing new error handling middleware

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express()
 const cors = require('cors')
 
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 
 
 //Middleware for console logs with info about petitions
@@ -18,37 +18,43 @@ morgan.token('post-body',(request,response)=>{
 })
 app.use(morgan(':method :url :status :response-time ms :post-body'))
 
-app.get('/api/persons', (request, response) => {
+
+//Request handlers
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons =>{
         response.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person =>{
         if (person) response.json(person)
         else response.status(404).end()
     })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.find({}).then(persons => {
         const date = new Date()
         let toPrint = `Phonebook has info for ${persons.length} people<br/>
         ${date}`
         response.send(toPrint)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request,response)=>{
+app.delete('/api/persons/:id', (request,response, next)=>{
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             console.log('Deleted: ',result.name)
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons',(request, response)=>{
+app.post('/api/persons',(request, response, next)=>{
     const body = request.body
     
     //Checks the request is good
@@ -73,9 +79,29 @@ app.post('/api/persons',(request, response)=>{
             }) 
         }
     })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
 })
+
+//Unknown endpoints middleware
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
+//Error handling middleware (needs to be last)
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+
+    next(error)
+}
+app.use(errorHandler)
